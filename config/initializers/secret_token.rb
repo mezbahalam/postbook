a6f1fdcc96115b36a58ed99ea40730310df635a3
mezbahalam <mezbahalam26@gmail.com>
2013-12-20 14:32:49 +0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Postbook::Application.config.secret_key_base = '37327e2c9029b513f03a3fa09f4ffd214081789e943cdbb2a753b41bc69c86cf89fe05bfef9d43d09eda9945ec72240fe869ce2ea6877d108a72985d6e21c557'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Postbook::Application.config.session_store :cookie_store, key: '_postbook_session'
